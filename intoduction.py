afd789bbbbaf2b8912b4876edb6246d5cdd60b46
rajoramayank482-cmyk <rajoramayank482@gmail.com>
2026-05-20 12:58:28 +0530

    Create intoduction.py

---
+++ intoduction.py
@@ -0,0 +1,9 @@
+name1 = "Sidharth"
+age = 18 
+name2 = "Mayank"
+Address = "Guriani" 
+
+print ("my name1 is : ", name1)
+print ("my age is :",age)
+print ("my name2 is :",name2)
+print ("my Address is :",Address)
