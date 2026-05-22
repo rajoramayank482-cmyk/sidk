e35d9be75b3b30ac8f01ccab37449faad1a6d263
rajoramayank482-cmyk <rajoramayank482@gmail.com>
2026-05-22 15:23:48 +0530

    Create calculation.py

---
+++ calculation.py
@@ -0,0 +1,31 @@
+#Sum of number
+def cal_sum(a,b,c,d,e):                
+    sum = a+b+c+d+e 
+    print(sum)
+    return sum
+    
+cal_sum(100,116,134,345,234)
+
+#sub of number
+def cal_sub(a,b):                
+    sub = a-b
+    print(sub)
+    return sub
+    
+cal_sub(345,126)
+
+#Multiplication of number
+def cal_mul(a,b,c,d):                
+    mul = a*b*c*d
+    print(mul)
+    return mul
+    
+cal_mul(100,116,134,345)
+
+#Division of number
+def cal_div(a,b):                
+    div = a/b
+    print(div)
+    return div
+    
+cal_div(100,25)
