c75ae15b69eb6f42047b000afe7c33cfba31b984
rajoramayank482-cmyk <rajoramayank482@gmail.com>
2026-05-25 10:15:46 +0530

    Create guess error.py

---
+++ guess error.py
@@ -0,0 +1,17 @@
+import random 
+
+number = random.randint(1, 50)
+
+print("Guess the number between 1 and 50")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    if guess>50:
+        print("invalid!")
+    if guess < number:
+        print(" low! Try again.")
+    elif guess > number:
+        print(" high! Try again.")
+    else:
+        print("Correct! You win ")
+        break
